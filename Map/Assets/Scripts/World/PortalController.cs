c4511d65067b272f98363fb4493140895570ab5a
inane <inanephenomenon@gmail.com>
2025-08-06 12:58:59 +0900

    Update PortalController.cs

--- Map/Assets/Scripts/World/PortalController.cs
+++ Map/Assets/Scripts/World/PortalController.cs
@@ -3,6 +3,21 @@ using UnityEngine;
 
 public class PortalController : MonoBehaviour
 {
-    [Tooltip("这个传送门的目标传送门的名字 (在 Hierarchy 中的名字)")]
-    public string targetPortalName;
+    [Tooltip("這個傳送門可傳送到的目標傳送門名字列表 (在 Hierarchy 中的名字)")]
+    public string[] targetPortalNames;
+
+    public string targetPortalName
+    {
+        get
+        {
+            if (targetPortalNames == null || targetPortalNames.Length == 0)
+            {
+                Debug.LogWarning("傳送門未配置目標傳送門");
+                return string.Empty;
+            }
+
+            int index = Random.Range(0, targetPortalNames.Length);
+            return targetPortalNames[index];
+        }
+    }   
 }
